67b41d6172be53a1b5acf833e680d5e2c58518c6
cdavii <c.davieskeogh@gmail.com>
2021-12-22 10:23:56 +0000

    Updated EmployeesSQL with a prepared statement to remove an employee based on their employee ID and EmployeesDAO with the corresponding method.

--- src/main/java/employee_csv_proj/controller/sql_queries/EmployeesSQL.java
+++ src/main/java/employee_csv_proj/controller/sql_queries/EmployeesSQL.java
@@ -5,7 +5,6 @@ package employee_csv_proj.controller.sql_queries;
  */
 public interface EmployeesSQL {
     String ADD_EMPLOYEE = "INSERT INTO employees (" +
-            "TableID," +
             "EmployeeID," +
             "NamePrefix," +
             "FirstName," +
@@ -16,9 +15,13 @@ public interface EmployeesSQL {
             "DateOfBirth," +
             "DateOfJoining," +
             "Salary) " +
-            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    String GET_ALL_EMPLOYEES = "SELECT * FROM employees";
+    String REMOVE_EMPLOYEE = "DELETE FROM employees WHERE EmployeeID= '?' ";
+
+    String GET_ALL_EMPLOYEES = "SELECT EmployeeID, NamePrefix, FirstName, MiddleInitial, LastName, " +
+            "Gender, Email, DateOfBirth, DateOfJoining, Salary FROM employees";
+
     // example
     String GET_AVG_SALARY = "SELECT AVG(Salary) FROM employees";
 }
