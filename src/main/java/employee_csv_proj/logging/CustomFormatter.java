8c13b8b7cf92d55fcf581a3aa39a0d06af71bf7a
Pavle <pavlesvrdlin@outlook.com>
2021-12-22 23:07:58 +0000

    methods from SystemLogger removed and formatting added for the date and time of each log

--- src/main/java/employee_csv_proj/logging/CustomFormatter.java
+++ src/main/java/employee_csv_proj/logging/CustomFormatter.java
@@ -1,11 +1,18 @@
 package employee_csv_proj.logging;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class CustomFormatter extends Formatter {
     @Override
     public String format(LogRecord record) {
-        return null;
+        return getDateTimeFormat() + " " + record.getLevel() + ": " + record.getMessage() + "\n";
+    }
+
+    private String getDateTimeFormat() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
     }
 }
