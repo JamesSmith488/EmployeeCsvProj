40c94da5ed16b90c74773c863fea976b30030447
cdavii <c.davieskeogh@gmail.com>
2021-12-22 10:00:05 +0000

    Added EmployeesDAO class

--- src/main/java/employee_csv_proj/model/EmployeesDAO.java
+++ src/main/java/employee_csv_proj/model/EmployeesDAO.java
@@ -1,7 +1,35 @@
 package employee_csv_proj.model;
 
+import employee_csv_proj.controller.ConnectionManager;
+import employee_csv_proj.controller.sql_queries.EmployeesSQL;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class EmployeesDAO {
 
+    public static void addEmployee(Employee employee) {
+        try {
+            Connection connection = ConnectionManager.dbConnection();
+            PreparedStatement employeePreparedStatement = connection.prepareStatement(EmployeesSQL.ADD_EMPLOYEE);
 
+            employeePreparedStatement.setInt(1, employee.getEmpId());
+            employeePreparedStatement.setString(2, employee.getNamePrefix());
+            employeePreparedStatement.setString(3, employee.getFirstName());
+            employeePreparedStatement.setString(4, String.valueOf(employee.getMiddleInitial()));
+            employeePreparedStatement.setString(5, employee.getLastName());
+            employeePreparedStatement.setString(6, String.valueOf(employee.getGender()));
+            employeePreparedStatement.setString(7, employee.getEmail());
+            employeePreparedStatement.setDate(8, Date.valueOf(employee.getDateOfBirth()));
+            employeePreparedStatement.setDate(9, Date.valueOf(employee.getDateOfJoining()));
+            employeePreparedStatement.setInt(10, employee.getSalary());
+            employeePreparedStatement.execute();
 
+            ConnectionManager.closeConnection(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
