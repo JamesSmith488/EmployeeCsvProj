99f8460bef85a6cd9c621b40789d3efdb6834178
ultamatewizard <james.lance.smith@hotmail.co.uk>
2021-12-23 17:28:25 +0000

    Updated logger initialisation in LoggerManager

--- src/main/java/employee_csv_proj/logging/LoggerManager.java
+++ src/main/java/employee_csv_proj/logging/LoggerManager.java
@@ -4,33 +4,54 @@ import java.io.IOException;
 import java.util.logging.*;
 
 public class LoggerManager {
+
     public static Logger systemLogger = Logger.getLogger("systemLogger");
+    public static Logger configLogger = Logger.getLogger("configLogger");
+    public static Logger dbInitialiserDaoLogger = Logger.getLogger("dbInitialiserDaoLogger");
     public static Logger employeesDaoLogger = Logger.getLogger("employeesDaoLogger");
     public static Logger connectionManagerLogger = Logger.getLogger("connectionManagerLogger");
     public static Logger employeeCsvParserLogger = Logger.getLogger("employeeCsvParserLogger");
     public static Logger duplicatesLogger = Logger.getLogger("duplicatesLogger");
 
-    public static void initialiseSystemLogger() {
+    public static void initialiseLoggers() {
+        initialiseSystemLogger();
+        initialiseConfigLogger();
+        initialiseDbInitialiserDaoLogger();
+        initialiseEmployeesDaoLogger();
+        initialiseConnectionManagerLogger();
+        initialiseEmployeeCsvParserLogger();
+        initialiseDuplicatesLogger();
+    }
+
+    private static void initialiseSystemLogger() {
         initialiseLogger(systemLogger, Level.ALL);
     }
 
-    public static void initialiseEmployeesDaoLogger() {
+    private static void initialiseConfigLogger() {
+        initialiseLogger(configLogger, Level.ALL);
+    }
+
+    private static void initialiseDbInitialiserDaoLogger() {
+        initialiseLogger(dbInitialiserDaoLogger, Level.ALL);
+    }
+
+    private static void initialiseEmployeesDaoLogger() {
         initialiseLogger(employeesDaoLogger, Level.ALL);
     }
 
-    public static void initialiseConnectionManagerLogger() {
+    private static void initialiseConnectionManagerLogger() {
         initialiseLogger(connectionManagerLogger, Level.ALL);
     }
 
-    public static void initialiseEmployeeCsvParserLogger() {
+    private static void initialiseEmployeeCsvParserLogger() {
         initialiseLogger(employeeCsvParserLogger, Level.ALL);
     }
 
-    public static void initialiseDuplicatesLogger() {
+    private static void initialiseDuplicatesLogger() {
         initialiseLogger(duplicatesLogger, Level.ALL);
     }
 
-    public static void initialiseLogger(Logger loggerName, Level logLevel) {
+    private static void initialiseLogger(Logger loggerName, Level logLevel) {
         try {
             Handler fileHandler = new FileHandler("src/main/logs/" + loggerName.getName() + ".log", true);
             fileHandler.setLevel(logLevel);
@@ -41,4 +62,5 @@ public class LoggerManager {
             e.printStackTrace();
         }
     }
+
 }
