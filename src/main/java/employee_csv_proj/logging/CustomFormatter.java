fd4f694d6e58b6db50418d1084a86d0abecda0b8
ultamatewizard <james.lance.smith@hotmail.co.uk>
2022-01-04 09:52:02 +0000

    Removed unnecessary import from CustomFormatter

--- src/main/java/employee_csv_proj/logging/CustomFormatter.java
+++ src/main/java/employee_csv_proj/logging/CustomFormatter.java
@@ -2,11 +2,11 @@ package employee_csv_proj.logging;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class CustomFormatter extends Formatter {
+
     @Override
     public String format(LogRecord record) {
         return getDateTimeFormat() + " " + record.getLevel() + ": " + record.getMessage() + "\n";
@@ -15,4 +15,5 @@ public class CustomFormatter extends Formatter {
     private String getDateTimeFormat() {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
     }
+
 }
